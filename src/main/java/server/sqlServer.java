7ec923ef5a2c5fc818998d242c176d4f7a4676ae
Sergey <losevsergio26@gmail.com>
2017-06-30 15:19:06 +0300

    2 step

--- src/main/java/server/sqlServer.java
+++ src/main/java/server/sqlServer.java
@@ -1,10 +1,43 @@
 package server;
 
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 /**
  * Created by User on 30.06.17.
  */
 public class sqlServer {
+    public static final String URL = "jdbc:mysql://localhost:3306/mydbtest?autoReconnect=true&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    public static final String USER = "root";
+    public static final String PASSWORD = "root";
+
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        Connection connection;
+        Driver driver;
+
+        try {
+
+            driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("V base");
+            }
+            connection.close();
+            if (connection.isClosed()) {
+                System.out.println("Vishel is base");
+            }
+        }catch (SQLException e){
+            System.out.println("ошибка драйвера SQL");
+            return;
+        }
+
     }
 }
