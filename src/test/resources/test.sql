85a0eefc901a4944b8020da1571967996595fc27
Sergey <losevsergio26@gmail.com>
2017-06-30 14:02:39 +0300

    2 step

---
+++ src/test/resources/test.sql
@@ -0,0 +1,6 @@
+#insert into users (name, age, email) values('asdad', 15, 'asdserjerk@mamsdl.psdfr');
+#select name, age from users where id = 1;
+#update users set name='Steve', age = 55 where id = 1;
+select * from users;
+#select * from users where name = 'Steve';
+#delete from users where id = 7;
